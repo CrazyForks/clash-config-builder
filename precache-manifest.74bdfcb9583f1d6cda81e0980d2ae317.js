3a77353ffdb8560ceb4d40d2e1c6a0e264d6aaaf
Fndroid <Fndroid@users.noreply.github.com>
2019-11-17 09:40:28 +0000

    Deploying to gh-pages - 09:40:28

---
+++ precache-manifest.74bdfcb9583f1d6cda81e0980d2ae317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa30a0e5c4927dec7c04ec7c44e8b0e0",
+    "url": "/clash-config-builder/index.html"
+  },
+  {
+    "revision": "c801fdca2c76b15b799c",
+    "url": "/clash-config-builder/static/css/2.44dc3df5.chunk.css"
+  },
+  {
+    "revision": "9b8eec1064e22ddb274a",
+    "url": "/clash-config-builder/static/css/main.d8efd439.chunk.css"
+  },
+  {
+    "revision": "c801fdca2c76b15b799c",
+    "url": "/clash-config-builder/static/js/2.9d34a8ca.chunk.js"
+  },
+  {
+    "revision": "9b8eec1064e22ddb274a",
+    "url": "/clash-config-builder/static/js/main.a3cf7fad.chunk.js"
+  },
+  {
+    "revision": "1c7de23e2cd09fa19f39",
+    "url": "/clash-config-builder/static/js/runtime-main.4b107252.js"
+  }
+]);
